d7856dbeb87f176ef7b23d2312178c85e04d22f2
DESKTOP-E7S6P6O\u <leesj9627@naver.com>
2026-07-29 20:19:06 +0900

    Initial commit: 초기파일 생성

---
+++ src/main/java/com/mamoki/ieojuda/IeojudaApplication.java
@@ -0,0 +1,13 @@
+package com.mamoki.ieojuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeojudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeojudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamoki/ieojuda/IeojudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamoki.ieojuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IeojudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
