9df8b4f107cdc52f00c2e90e29e2b71861e475df
이민구 <minet0401@gmail.com>
2026-08-01 21:28:36 +0900

    [Feat/#1]:역할 담당자 종류 enum

---
+++ src/main/java/com/mamoki/ieojuda/recipient/entity/RoleType.java
@@ -0,0 +1,7 @@
+package com.mamoki.ieojuda.recipient.entity;
+
+public enum RoleType {
+    FAMILY_MANAGER,       // 가족 담당자
+    WORK_MANAGER,         // 업무 담당자
+    RELATIONSHIP_MANAGER  // 관계 정리 담당자
+}
