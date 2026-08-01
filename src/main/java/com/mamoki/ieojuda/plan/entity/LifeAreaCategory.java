9e6b9e0ac28cc8df6529e760d7b7873693b0b2f8
이민구 <minet0401@gmail.com>
2026-08-01 21:26:00 +0900

    [Feat/#1]: 삶의 구역 카테고리 enum

---
+++ src/main/java/com/mamoki/ieojuda/plan/entity/LifeAreaCategory.java
@@ -0,0 +1,7 @@
+package com.mamoki.ieojuda.plan.entity;
+
+public enum LifeAreaCategory {
+    FAMILY,               // 가족
+    RELATIONSHIP_CLEANUP, // 관계 정리
+    WORK_CONTINUITY       // 업무 연속성
+}
