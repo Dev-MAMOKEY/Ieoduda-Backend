dd5d702f20d1a62d24a71dc7d5c5da48bb5a7f52
이민구 <minet0401@gmail.com>
2026-08-01 21:37:05 +0900

    [Feat/#1]:external_partners 테이블 매핑

---
+++ src/main/java/com/mamoki/ieojuda/partner/entity/ExternalPartner.java
@@ -0,0 +1,36 @@
+package com.mamoki.ieojuda.partner.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "external_partners")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExternalPartner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "partner_id")
+    private Long partnerId;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "partner_type", length = 30)
+    private PartnerType partnerType;
+
+    @Column(name = "contact_info", length = 255)
+    private String contactInfo;
+
+    @Builder
+    public ExternalPartner(String name, PartnerType partnerType, String contactInfo) {
+        this.name = name;
+        this.partnerType = partnerType;
+        this.contactInfo = contactInfo;
+    }
+}
