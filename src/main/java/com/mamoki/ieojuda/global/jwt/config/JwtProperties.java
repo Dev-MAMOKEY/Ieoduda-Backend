f395e07814541a1129923729bf0cc9cfd30b86ea
이수종 <leesj9627@naver.com>
2026-08-09 15:36:17 +0900

    [Feat/#9]: JwtProperties, Jwtprovider 구현

---
+++ src/main/java/com/mamoki/ieojuda/global/jwt/config/JwtProperties.java
@@ -0,0 +1,21 @@
+package com.mamoki.ieojuda.global.jwt.config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@NoArgsConstructor
+@Component
+public class JwtProperties {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access-token-expiration-ms}")
+    private long accessTokenExpirationMs;
+
+    @Value("${jwt.refresh-token-expiration-ms}")
+    private long refreshTokenExpirationMs;
+}
