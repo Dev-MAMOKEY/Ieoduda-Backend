89368fb420bbc1cef3b7ed3af9b74132904575c0
DESKTOP-E7S6P6O\u <leesj9627@naver.com>
2026-08-16 23:41:41 +0900

    [Refactor/#42]: 시간별 불변 계획 스냅샷 구현

--- src/main/java/com/mamoki/ieojuda/domain/plan/repository/PlanVersionRepository.java
+++ src/main/java/com/mamoki/ieojuda/domain/plan/repository/PlanVersionRepository.java
@@ -4,4 +4,7 @@ import com.mamoki.ieojuda.domain.plan.entity.PlanVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlanVersionRepository extends JpaRepository<PlanVersion, Long> {
+
+    // 사망 신고 접수 시 다음 버전 번호를 매기기 위한 카운트
+    long countByPlan_PlanId(Long planId);
 }
